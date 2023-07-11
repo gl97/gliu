55af73d988185ba93b0d2c957fcf626a0ddc9989
Gary Liu <liug1997@gmail.com>
2023-07-11 07:53:20 +0000

    push to main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export function App(props) {
+  return (
+    <div className='App'>
+      <h1>Hello React.</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button>Do something</button>
+    </div>
+  );
+}
+
+console.log('Hello console')
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { App } from './App.jsx'
+import './style.css';
+
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(<App />)
